fddb165bd313c72f4baf8d712e0ea60cf83d9514
ax-va <vasiliev.informatik@t-online.de>
2026-01-03 00:40:21 +0100

    update

--- input_eth.py
+++ input_eth.py
@@ -6,8 +6,8 @@ class Mode(Enum):
     HUNTER = "HUNTER"
 
 
-def detect_mode(price, vacuum_limit, battery_upper) -> Mode:
-    if price <= vacuum_limit:
+def detect_mode(price, vacuum_upper, battery_upper) -> Mode:
+    if price <= vacuum_upper:
         return Mode.VACUUM
     elif price <= battery_upper:
         return Mode.BATTERY
@@ -19,24 +19,26 @@ if __name__ == "__main__":
     local_high = 4000
     profit_p = 0.2  # take profit
     loss_p = profit_p * 2
-    vacuum_limit = local_high * (1 - loss_p)
-    print("vacuum_limit:", vacuum_limit)
+    vacuum_upper = local_high * (1 - loss_p)
+    print("vacuum_upper:", vacuum_upper)
 
     etp_price_bid = 2610  # sell
     etp_price_ask = 2613  # buy
     etp_price_spread = etp_price_ask - etp_price_bid
     print("etp_price_spread:", etp_price_spread)
 
-    battery_upper = vacuum_limit / (1 - profit_p)
+    battery_upper = vacuum_upper / (1 - profit_p)
     print("battery_upper:", battery_upper)
 
-    mode = detect_mode(etp_price_ask, vacuum_limit, battery_upper)
+    mode = detect_mode(etp_price_ask, vacuum_upper, battery_upper)
     print("mode:", mode)
 
     limit_order = etp_price_ask * (1 + profit_p)
     print("limit_order:", limit_order)
 
-    stop_loss = max(vacuum_limit, etp_price_ask * (1 - loss_p))
+    stop_loss_raw = etp_price_ask * (1 - loss_p)
+    print("stop_loss_raw:", stop_loss_raw)
+    stop_loss = max(vacuum_upper, stop_loss_raw)
     print("stop_loss:", stop_loss)
 
     cash_input = 0

1015dd56db459bc824ab2820b9c9fc6ae24a32e8
ax-va <vasiliev.informatik@t-online.de>
2026-01-08 02:38:47 +0100

    update

--- src/finanzmaschine/strategies/shares_to_cash_or_assets/share_lot_context.py
+++ src/finanzmaschine/strategies/shares_to_cash_or_assets/share_lot_context.py
@@ -15,32 +15,36 @@ class ShareLotContext:
     def __init__(
         self,
         share_lot: ShareLot,
-        asset_local_high: float,
         asset_profit_pct: float | None = 0.2,  # +20%
         asset_loss_pct: float | None = 0.4,  # -40%
-        asset_vac_upper_bound: float = None,
-        asset_acc_upper_bound: float = None,
+        asset_local_high: float | None = None,
+        asset_vac_upper_bound: float | None = None,
+        asset_acc_upper_bound: float | None = None,
     ):
-        assert asset_local_high > 0
         if asset_profit_pct is not None:
             assert asset_profit_pct > 0
         if asset_loss_pct is not None:
             assert 0 < asset_loss_pct < 1
-        if asset_vac_upper_bound is not None:
+        if asset_local_high is not None:
+            assert asset_local_high > 0
+        if asset_vac_upper_bound is not None and asset_local_high is not None:
             assert 0 < asset_vac_upper_bound < asset_local_high
-        if asset_acc_upper_bound is not None:
+        if asset_acc_upper_bound is not None and asset_local_high is not None:
             assert 0 < asset_acc_upper_bound < asset_local_high
         if asset_vac_upper_bound is not None and asset_acc_upper_bound is not None:
             assert asset_vac_upper_bound <= asset_acc_upper_bound
 
         self.share_lot: ShareLot = share_lot
-        self.asset_local_high: float = asset_local_high
+        self.asset_local_high: float | None = asset_local_high
         self.asset_profit_pct: float | None = asset_profit_pct
         self.asset_loss_pct: float | None = asset_loss_pct
 
         # VACUUM mode upper bound for asset price
         if asset_vac_upper_bound is None:
-            if asset_loss_pct is not None:
+            if (
+                self.asset_local_high is not None
+                and asset_loss_pct is not None
+            ):
                 asset_vac_upper_bound = self.asset_local_high * (1 - self.asset_loss_pct)
         self.asset_vac_upper_bound: float | None = asset_vac_upper_bound
 

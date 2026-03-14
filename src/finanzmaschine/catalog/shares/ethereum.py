b279b5445eedb12cc860313fc31351871509ba79
ax-va <vasiliev.informatik@t-online.de>
2026-03-14 21:51:09 +0100

    update

--- src/finanzmaschine/catalog/shares/ethereum.py
+++ src/finanzmaschine/catalog/shares/ethereum.py
@@ -3,7 +3,7 @@ from finanzmaschine.catalog.instrument_registry import registry
 from finanzmaschine.core.market.share import Share
 
 
-ETP_ETH_COINSHARES = registry.register(
+ETH_COINSHARES = registry.register(
     Share(
         isin="GB00BLD4ZM24",
         name="CoinShares Physical Staked Ethereum",

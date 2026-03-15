3799740b93f60e6273eed9489ba7cf8e6a01dcb3
ax-va <vasiliev.informatik@t-online.de>
2026-03-15 03:09:12 +0100

    update

--- src/finanzmaschine/catalog/shares/ethereum.py
+++ src/finanzmaschine/catalog/shares/ethereum.py
@@ -3,7 +3,7 @@ from finanzmaschine.catalog.instrument_registry import registry
 from finanzmaschine.core.market.share import Share
 
 
-ETH_ETP_BY_COINSHARES = registry.register(
+registry.register(
     Share(
         isin="GB00BLD4ZM24",
         name="CoinShares Physical Staked Ethereum",
--- src/finanzmaschine/catalog/shares/toncoin.py
+++ src/finanzmaschine/catalog/shares/toncoin.py
@@ -3,7 +3,7 @@ from finanzmaschine.catalog.instrument_registry import registry
 from finanzmaschine.core.market.share import Share
 
 
-TON_ETP_BY_21SHARES = registry.register(
+registry.register(
     Share(
         isin="CH1297762812",
         name="21Shares Toncoin Staking ETP",
